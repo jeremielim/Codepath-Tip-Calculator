f7ff48b9a43756c15a01b7e64981e30246508cb8
Jeremie Michaels Lim <hello@jeremiespoken.com>
2016-01-06 20:27:54 -0800

    added initial UI for tip calculator

--- codepath-tip-calculator/codepath-tip-calculator/ViewController.swift
+++ codepath-tip-calculator/codepath-tip-calculator/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var billAmount = Double(billField.text!)
+        var tip = billAmount! * 0.2
+        var total = billAmount! + tip
+        
+        tipLabel.text = String(tip)
+        
+        
+    }
 
 }
 
