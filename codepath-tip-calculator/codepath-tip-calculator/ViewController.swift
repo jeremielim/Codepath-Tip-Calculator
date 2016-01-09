d84655ee0e75835727d98388101c0959dc8c4b4f
Jeremie Michaels Lim <hello@jeremiespoken.com>
2016-01-08 19:26:06 -0800

    Completed initial tip calculator

--- codepath-tip-calculator/codepath-tip-calculator/ViewController.swift
+++ codepath-tip-calculator/codepath-tip-calculator/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,15 +30,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
     @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18,0.2,0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
-        var billAmount = Double(billField.text!)
-        var tip = billAmount! * 0.2
-        var total = billAmount! + tip
+        print(tipPercentage)
         
-        tipLabel.text = String(tip)
+        let billAmount = Double(billField.text!)
+        let tip = billAmount! * tipPercentage
+        let total = billAmount! + tip
         
-        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
 
 }
