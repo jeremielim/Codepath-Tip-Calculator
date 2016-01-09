0ee4f84a5e5120d0fed0f2e75e028edc28cdf23c
Jeremie Michaels Lim <hello@jeremiespoken.com>
2016-01-09 14:32:00 -0800

    Added custom font and background colors

--- codepath-tip-calculator/codepath-tip-calculator/ViewController.swift
+++ codepath-tip-calculator/codepath-tip-calculator/ViewController.swift
@@ -22,31 +22,52 @@ class ViewController: UIViewController {
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
         
-        
+        // Autofocus keyboard on load
+        self.billField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Changing Status Bar Color to White
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        
+        //LightContent
+        return UIStatusBarStyle.LightContent
+        
+    }
 
+    // Hide Keyboard When Clicking Outside of Form
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
     
+    
+    // Calculate Tip
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.18,0.2,0.22]
-        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
-        print(tipPercentage)
-        
-        let billAmount = Double(billField.text!)
-        let tip = billAmount! * tipPercentage
-        let total = billAmount! + tip
-        
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        // Check if bill total field is not empty
+        if billField.text != "" {
+    
+            // Get percentage amount
+            var tipPercentages = [0.18,0.2,0.22]
+            let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+            
+            let billAmount = Double(billField.text!)
+            let tip = billAmount! * tipPercentage
+            let total = billAmount! + tip
+            
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+        } else {
+            // Reset tip and total labels
+            tipLabel.text = "$0.00"
+            totalLabel.text = "$0.00"
+        }
     }
+    
 
 }
 
